84ff5b2134903579c134f4fc5cff030b2764915d
Roman Zenka <zenka.roman@mayo.edu>
2013-02-05 11:54:53 -0600

    I do not understand this stuff, remove for now

--- spec/defines/logstash__initscript_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'logstash::initscript', :type => :define do
-  let(:title)  { 'logstash-indexer' }
-  let(:params) { { :config => '/etc/logstash/indexer.conf' } }
-
-  it { should contain_file('logstash-indexer')}
-end
--- spec/defines/logstash__service_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe 'logstash::service', :type => :define do
-  let(:title) { 'logstash-indexer' }
-
-  it { should contain_service('logstash-indexer') }
-end
